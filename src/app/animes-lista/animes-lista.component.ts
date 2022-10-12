dcea958d6f6aca2b4b045b9d2f7576fb151a9347
MEzequielFB <ezequiel_frisone@hotmail.com>
2022-10-12 11:14:46 -0300

    Agrega pagina de inicio con navegacion y footer

---
+++ src/app/animes-lista/animes-lista.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-animes-lista',
+  templateUrl: './animes-lista.component.html',
+  styleUrls: ['./animes-lista.component.scss']
+})
+export class AnimesListaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { FooterComponent } from './footer/footer.component';
+import { AnimesListaComponent } from './animes-lista/animes-lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    InicioComponent,
+    FooterComponent,
+    AnimesListaComponent
   ],
   imports: [
     BrowserModule,
